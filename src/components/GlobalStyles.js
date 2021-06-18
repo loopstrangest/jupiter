fcbf7dc9aaad50535b7a14703412dd207b81410b
Dalton Dinderman <loopstrangest@gmail.com>
2021-06-17 21:34:05 -0500

    Removed 'X' from Chrome search input

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -35,6 +35,15 @@ p{
 a{
     text-decoration: none;
 }
+/* clear the 'X' from Internet Explorer */
+input[type=search]::-ms-clear {  display: none; width : 0; height: 0; }
+input[type=search]::-ms-reveal {  display: none; width : 0; height: 0; }
+
+/* clear the 'X' from Chrome */
+input[type="search"]::-webkit-search-decoration,
+input[type="search"]::-webkit-search-cancel-button,
+input[type="search"]::-webkit-search-results-button,
+input[type="search"]::-webkit-search-results-decoration { display: none; }
 `;
 
 export default GlobalStyles;
